9c739f9ff650f06c478a774aedab248a2f13f5bc
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2025-09-30 09:53:32 +0100

    Add nginx manifests and basic ArgoCD setup guide

---
+++ README.md
@@ -0,0 +1,7 @@
+# Prerequisites
+
+- [Kubectl](https://kubernetes.io/docs/tasks/tools/#kubectl)
+- A local K8s cluster. I use [kind](https://kind.sigs.k8s.io/docs/user/quick-start/#installation) to create one but you can use minikube or whatever else you feel comfortable with. Here are some [examples](https://kubernetes.io/docs/tasks/tools/).
+- Install [ArgoCD docs](https://argo-cd.readthedocs.io/en/stable/operator-manual/installation/)
+
+
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+kind create cluster --name argocd
+
+kubectl create namespace argocd
+kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/refs/tags/stable/manifests/install.yaml
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+kubectl port-forward svc/argocd-server -n argocd 8080:80
+
+# open http://localhost:8080 in your browser
+# username: admin
+# password: <this_is_the_password_you_just_got>
